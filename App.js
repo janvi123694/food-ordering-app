83d6f96cb094f205a7b512faa914bf8e80053a50
Sai <tsaijanhavi23@gmail.com>
2024-02-06 17:13:57 -0500

    rendering dynamic content

---
+++ App.js
@@ -0,0 +1,598 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Header = () => {
+    return (
+    <div className="header">
+        <div className="logo-container">
+            <img className="logo" src="https://dynamic.brandcrowd.com/asset/logo/7f982a19-779d-4dd3-b533-7a9f66474000/logo-search-grid-1x?logoTemplateVersion=1&v=637810055012670000"></img>
+        </div>
+        <div className="nav-items">
+            <ul>
+                <li>Homee</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    </div>
+    )
+}
+
+const resList = [
+    {
+    "info": {
+    "id": "179722",
+    "name": "Chai Kings",
+    "cloudinaryImageId": "kvqwhjkh24w9w69aiwmj",
+    "locality": "Mogapair",
+    "areaName": "Mogappair",
+    "costForTwo": "₹150 for two",
+    "cuisines": [
+    "Beverages",
+    "Snacks",
+    "Bakery"
+    ],
+    "avgRating": 4.5,
+    "parentId": "2627",
+    "avgRatingString": "4.5",
+    "totalRatingsString": "5K+",
+    "sla": {
+    "deliveryTime": 22,
+    "lastMileTravel": 2.6,
+    "serviceability": "SERVICEABLE",
+    "slaString": "22 mins",
+    "lastMileTravelString": "2.6 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-02-07 05:59:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "40% OFF",
+    "subHeader": "UPTO ₹80"
+    },
+    "loyaltyDiscoverPresentationInfo": {
+    "logoCtx": {
+    "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+    },
+    "freedelMessage": "FREE DELIVERY",
+    "badgeType": "BADGE_TYPE_ONE_LITE"
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {},
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/chai-kings-mogapair-mogappair-chennai-179722",
+    "type": "WEBLINK"
+    }
+    },
+    {
+    "info": {
+    "id": "13295",
+    "name": "Subway",
+    "cloudinaryImageId": "63178e3e64d503a479f2a2048a474552",
+    "locality": "Valsarvakkam",
+    "areaName": "Porur",
+    "costForTwo": "₹350 for two",
+    "cuisines": [
+    "Salads",
+    "Snacks",
+    "Desserts",
+    "Beverages"
+    ],
+    "avgRating": 4.1,
+    "parentId": "2",
+    "avgRatingString": "4.1",
+    "totalRatingsString": "10K+",
+    "sla": {
+    "deliveryTime": 27,
+    "lastMileTravel": 4.2,
+    "serviceability": "SERVICEABLE",
+    "slaString": "27 mins",
+    "lastMileTravelString": "4.2 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-02-07 05:59:00",
+    "opened": true
+    },
+    "badges": {
+    "textExtendedBadges": [
+    {
+    "iconId": "guiltfree/GF_Logo_android_3x",
+    "shortDescription": "options available",
+    "fontColor": "#7E808C"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "",
+    "fontColor": "#7E808C",
+    "iconId": "guiltfree/GF_Logo_android_3x",
+    "shortDescription": "options available"
+    }
+    }
+    ]
+    }
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "₹100 OFF",
+    "subHeader": "ABOVE ₹249",
+    "discountTag": "FLAT DEAL"
+    },
+    "loyaltyDiscoverPresentationInfo": {
+    "logoCtx": {
+    "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+    },
+    "freedelMessage": "FREE DELIVERY",
+    "badgeType": "BADGE_TYPE_ONE_LITE"
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {},
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/subway-valsarvakkam-porur-chennai-13295",
+    "type": "WEBLINK"
+    }
+    },
+    {
+    "info": {
+    "id": "44246",
+    "name": "Roll Baby Roll",
+    "cloudinaryImageId": "egixe8rmkmd5utdjqxl0",
+    "locality": "Near Ice cream works",
+    "areaName": "Annanagar East",
+    "costForTwo": "₹250 for two",
+    "cuisines": [
+    "Indian",
+    "Rolls & Wraps",
+    "Fast Food",
+    "Street Food",
+    "Snacks",
+    "Beverages",
+    "Juices"
+    ],
+    "avgRating": 4.4,
+    "parentId": "4228",
+    "avgRatingString": "4.4",
+    "totalRatingsString": "10K+",
+    "sla": {
+    "deliveryTime": 27,
+    "lastMileTravel": 7.3,
+    "serviceability": "SERVICEABLE",
+    "slaString": "27 mins",
+    "lastMileTravelString": "7.3 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-02-07 04:50:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "50% OFF",
+    "subHeader": "UPTO ₹100"
+    },
+    "loyaltyDiscoverPresentationInfo": {
+    "logoCtx": {
+    "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+    },
+    "freedelMessage": "FREE DELIVERY",
+    "badgeType": "BADGE_TYPE_ONE_LITE"
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {},
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/roll-baby-roll-near-ice-cream-works-annanagar-east-chennai-44246",
+    "type": "WEBLINK"
+    }
+    },
+    {
+    "info": {
+    "id": "504619",
+    "name": "Junior Kuppanna",
+    "cloudinaryImageId": "4d04a257d504a5083c9dd59d14c8c612",
+    "locality": "Anna Nagar",
+    "areaName": "Anna Nagar",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+    "Biryani"
+    ],
+    "avgRating": 4,
+    "parentId": "112209",
+    "avgRatingString": "4.0",
+    "totalRatingsString": "1K+",
+    "sla": {
+    "deliveryTime": 34,
+    "lastMileTravel": 7.3,
+    "serviceability": "SERVICEABLE",
+    "slaString": "34 mins",
+    "lastMileTravelString": "7.3 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-02-07 05:59:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "ITEMS",
+    "subHeader": "AT ₹249"
+    },
+    "loyaltyDiscoverPresentationInfo": {
+    "logoCtx": {
+    "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+    },
+    "freedelMessage": "FREE DELIVERY",
+    "badgeType": "BADGE_TYPE_ONE_LITE"
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {},
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/junior-kuppanna-anna-nagar-chennai-504619",
+    "type": "WEBLINK"
+    }
+    },
+    {
+    "info": {
+    "id": "13301",
+    "name": "Meat and Eat",
+    "cloudinaryImageId": "pj93hcolhatk8dycodu4",
+    "locality": "Chinmaya Nagar Stage 1",
+    "areaName": "Vadapalani",
+    "costForTwo": "₹250 for two",
+    "cuisines": [
+    "Beverages",
+    "Fast Food",
+    "Burgers"
+    ],
+    "avgRating": 4,
+    "parentId": "632",
+    "avgRatingString": "4.0",
+    "totalRatingsString": "1K+",
+    "sla": {
+    "deliveryTime": 33,
+    "lastMileTravel": 4.4,
+    "serviceability": "SERVICEABLE",
+    "slaString": "33 mins",
+    "lastMileTravelString": "4.4 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-02-07 05:59:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "40% OFF",
+    "subHeader": "UPTO ₹80"
+    },
+    "loyaltyDiscoverPresentationInfo": {
+    "logoCtx": {
+    "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+    },
+    "freedelMessage": "FREE DELIVERY",
+    "badgeType": "BADGE_TYPE_ONE_LITE"
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {},
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/meat-and-eat-chinmaya-nagar-stage-1-vadapalani-chennai-13301",
+    "type": "WEBLINK"
+    }
+    },
+    {
+    "info": {
+    "id": "50216",
+    "name": "Drunken Monkey",
+    "cloudinaryImageId": "2d0c902eb2975b651d78e566bd8d06be",
+    "locality": "Sterling Road",
+    "areaName": "Nungambakkam",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+    "Healthy Food",
+    "Beverages",
+    "Juices"
+    ],
+    "avgRating": 4.4,
+    "parentId": "1729",
+    "avgRatingString": "4.4",
+    "totalRatingsString": "5K+",
+    "sla": {
+    "deliveryTime": 37,
+    "lastMileTravel": 8.4,
+    "serviceability": "SERVICEABLE",
+    "slaString": "37 mins",
+    "lastMileTravelString": "8.4 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-02-07 05:59:00",
+    "opened": true
+    },
+    "badges": {
+    "textExtendedBadges": [
+    {
+    "iconId": "guiltfree/GF_Logo_android_3x",
+    "shortDescription": "options available",
+    "fontColor": "#7E808C"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "",
+    "fontColor": "#7E808C",
+    "iconId": "guiltfree/GF_Logo_android_3x",
+    "shortDescription": "options available"
+    }
+    }
+    ]
+    }
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "10% OFF",
+    "subHeader": "ABOVE ₹1900",
+    "discountTag": "FLAT DEAL"
+    },
+    "loyaltyDiscoverPresentationInfo": {
+    "logoCtx": {
+    "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+    },
+    "freedelMessage": "FREE DELIVERY",
+    "badgeType": "BADGE_TYPE_ONE_LITE"
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {},
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/drunken-monkey-sterling-road-nungambakkam-chennai-50216",
+    "type": "WEBLINK"
+    }
+    },
+    {
+    "info": {
+    "id": "14108",
+    "name": "Sushi In A Box",
+    "cloudinaryImageId": "hhqiy8yhbqwebimiphf1",
+    "locality": "Opp Lanson Toyota",
+    "areaName": "Alwarpet",
+    "costForTwo": "₹999 for two",
+    "cuisines": [
+    "Sushi",
+    "Japanese",
+    "Chinese"
+    ],
+    "avgRating": 4.4,
+    "parentId": "198069",
+    "avgRatingString": "4.4",
+    "totalRatingsString": "1K+",
+    "sla": {
+    "deliveryTime": 50,
+    "lastMileTravel": 11.5,
+    "serviceability": "SERVICEABLE",
+    "slaString": "50 mins",
+    "lastMileTravelString": "11.5 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-02-07 05:59:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "newg.png",
+    "description": "Gourmet"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "Gourmet",
+    "imageId": "newg.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "40% OFF",
+    "subHeader": "UPTO ₹80"
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {},
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/sushi-in-a-box-opp-lanson-toyota-alwarpet-chennai-14108",
+    "type": "WEBLINK"
+    }
+    }
+    ]
+const RestaurantCard=(props)=>{
+    const {resData} = props;
+    const {name, cuisines, cloudinaryImageId, avgRating, costForTwo} = resData?.info;
+    const {deliveryTime} = resData?.info?.sla;
+
+   return (
+    <div className="res-card" style={{background:"#f0f0f0"}}>
+        <img className='logo' src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`}/>
+        <h3>{name}</h3>
+        <h4>{cuisines.join(",")}</h4>
+        <h4>{costForTwo}</h4>
+        <h4>{deliveryTime}</h4>
+    </div>
+   )
+}
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search"></div>
+            <div className="res-container">
+                {
+                    resList.map((resData, index) => <RestaurantCard key={index} resData={resData}/>)
+                    
+                }
+                
+            </div>
+        </div>
+    )
+}
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+    )
+}
+
+
+ReactDOM.createRoot(
+    document.getElementById("root"),
+  )
+  .render(
+    <React.StrictMode>
+      <AppLayout />
+    </React.StrictMode>,
+  );
